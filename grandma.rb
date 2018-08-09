15b38e4d4606ae21e5945c48a411b2155e473da2
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 00:45:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    return ""
+  elsif phrase == phrase.upcase
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
